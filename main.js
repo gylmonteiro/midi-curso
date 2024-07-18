699806f053090d8e59821ae16cf4f6e89bcb7c5d
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-07-18 15:12:48 -0300

    feat: criado código Javascript para manipular o Html, Css e acionar sons

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
-function tocaSom (seletorAudio) {
-    const elemento = document.querySelector(seletorAudio);
-
-    if (elemento && elemento.localName === 'audio') {
-        elemento.play();
+function tocarSom (idElementAudio) {
+    const elemento = document.querySelector(idElementAudio)
+    
+    if (elemento != null && elemento.localName === 'audio'){
+        elemento.play()
     }
     else {
-        //alert('Elemento não encontrado');
-        console.log('Elemento não encontrado ou seletor inválido');
+        console.log('Comando inválido')
     }
-
+    
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll(".tecla")
 
-//para
+
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla =  listaDeTeclas[contador]
+    const classTecla = tecla.classList[1]
 
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
+    // Template string
+    const idAudioCompleto = `#som_${classTecla}`
 
     tecla.onclick = function () {
-        tocaSom(idAudio);
+        tocarSom(idAudioCompleto);
     }
 
+    
     tecla.onkeydown = function (evento) {
+        if(evento.code == 'Enter' || evento.code == 'Space'){
 
-        if (evento.code === 'Space' || evento.code === 'Enter') {
-            tecla.classList.add('ativa');
+            tecla.classList.add('ativa')
         }
-
     }
 
     tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
+        tecla.classList.remove('ativa')
     }
-
-}
+    }
